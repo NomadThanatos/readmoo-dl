ad94a2dbe572d392d4553f413ce0ddbc86e86aad
David Liu <msglight4874@gmail.com>
2018-12-29 18:39:28 +0800

    remove unused header

--- lib/readmoo_dl/API.rb
+++ lib/readmoo_dl/API.rb
@@ -37,7 +37,6 @@ module ReadmooDL
         :'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) ' \
                        'AppleWebKit/537.36 (KHTML, like Gecko) ' \
                        'Chrome/71.0.3578.98 Safari/537.36',
-        :'accept' => 'application/json, text/javascript, */*; q=0.01',
         :'x-requested-with' => 'XMLHttpRequest'
       }
     end
