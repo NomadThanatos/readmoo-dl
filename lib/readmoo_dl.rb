ac4a9f39dc342821945388646910ed694f8ab2b5
David Liu <msglight4874@gmail.com>
2018-12-29 15:49:34 +0800

    remove trail slash

--- lib/readmoo_dl.rb
+++ lib/readmoo_dl.rb
@@ -3,7 +3,7 @@ require 'bundler'
 Bundler.require(:default)
 
 module ReadmooDL
-  LOGIN_URL = 'https://member.readmoo.com/login/'
+  LOGIN_URL = 'https://member.readmoo.com/login'
   API_URL = 'https://reader.readmoo.com'
 end
 
