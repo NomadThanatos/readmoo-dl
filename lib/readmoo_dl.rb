1852d1145e69554d2e79eb440ace51ebb1226ce8
David Liu <msglight4874@gmail.com>
2018-12-29 17:37:21 +0800

    add files class auto require

--- lib/readmoo_dl.rb
+++ lib/readmoo_dl.rb
@@ -2,9 +2,15 @@ require 'rubygems'
 require 'bundler'
 Bundler.require(:default)
 
+require 'json'
+
 module ReadmooDL
   LOGIN_URL = 'https://member.readmoo.com/login'
   API_URL = 'https://reader.readmoo.com'
 end
 
 require_relative './readmoo_dl/API'
+require_relative './readmoo_dl/downloader'
+require_relative './readmoo_dl/file'
+
+Dir[File.join(__dir__, 'readmoo_dl', 'files', '*.rb')].each(&method(:require))
