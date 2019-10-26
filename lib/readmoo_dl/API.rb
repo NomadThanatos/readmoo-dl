d4333d3c4580dfb514df2091f169dd0538d33e82
David Liu <msglight4874@gmail.com>
2019-10-27 03:05:20 +0800

    修正 Readmoo 會檢查 referer header

--- lib/readmoo_dl/API.rb
+++ lib/readmoo_dl/API.rb
@@ -37,7 +37,8 @@ module ReadmooDL
         :'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) ' \
                        'AppleWebKit/537.36 (KHTML, like Gecko) ' \
                        'Chrome/71.0.3578.98 Safari/537.36',
-        :'x-requested-with' => 'XMLHttpRequest'
+        :'x-requested-with' => 'XMLHttpRequest',
+        :'referer' => 'https://reader.readmoo.com/reader/index.html',
       }
     end
 
